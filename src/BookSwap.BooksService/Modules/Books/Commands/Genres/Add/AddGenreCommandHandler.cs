d5a2e02cb75e725b1cece59a771006ecc9fdeda0
Rafael Cristiano da Silva <rafael.silva@sellersfunding.com>
2023-09-21 15:51:05 -0300

    Add cache support to list genres route

--- src/BookSwap.BooksService/Modules/Books/Commands/Genres/Add/AddGenreCommandHandler.cs
+++ src/BookSwap.BooksService/Modules/Books/Commands/Genres/Add/AddGenreCommandHandler.cs
@@ -1,15 +1,24 @@
 ﻿using BookSwap.BooksService.Modules.Books.Entities;
 using BookSwap.BooksService.Modules.Books.Interfaces;
+using BookSwap.Shared.Core.Mediator;
 using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace BookSwap.BooksService.Modules.Books.Commands.Genres.Add;
 
-public record AddGenreCommandHandler(IGenreRepository GenreRepository) : IRequestHandler<AddGenreCommand, Guid>
+public record AddGenreCommandHandler(
+    IGenreRepository GenreRepository, 
+    IDistributedCache DistributedCache
+) : IRequestHandler<AddGenreCommand, Guid>
 {
     public async Task<Guid> Handle(AddGenreCommand request, CancellationToken cancellationToken)
     {
         var genre = new BookGenre(request.Name, request.Description);
+        
         await GenreRepository.AddAsync(genre);
+
+        await DistributedCache.RemoveAsync($"genres/list", cancellationToken);
+        
         return genre.Id;
     }
 }
--- src/BookSwap.BooksService/Modules/Books/Queries/Genres/List/ListBookGenreQuery.cs
+++ src/BookSwap.BooksService/Modules/Books/Queries/Genres/List/ListBookGenreQuery.cs
@@ -1,6 +1,12 @@
 ﻿using BookSwap.BooksService.Modules.Books.Entities;
+using BookSwap.Shared.Core.Mediator;
 using MediatR;
 
 namespace BookSwap.BooksService.Modules.Books.Queries.Genres.List;
 
-public record ListBookGenreQuery(string? Name) : IRequest<IEnumerable<BookGenre>>;
+public record ListBookGenreQuery(string? Name) : IRequest<IEnumerable<BookGenre>>, ICacheableRequest
+{
+    public string CacheKey => $"genres/list";
+    public bool BypassCache => string.IsNullOrEmpty(Name); //Use cache only when list all genres
+    public TimeSpan SlidingExpiration => TimeSpan.FromDays(1);
+}
